099c7eb8f4c7c9ac1f7342403b188f37fbfc5a67
elisewilkinson <106769230+elisewilkinson@users.noreply.github.com>
2022-08-28 23:30:25 +0100

    Update script.js

--- geordie/imusic-player/script.js
+++ geordie/imusic-player/script.js
@@ -33,7 +33,7 @@ $(document).ready(function () {
 			title: "Smooth Criminal",
 			artist: "Busta, Micheal Jackson",
 			cover: "https://wilkinson.b-cdn.net/images/busta-micheal-jackson2.jpg",
-			audioFile: "https://wilkinson.b-cdn.net/busta.mp3",
+			audioFile: "https://dan.wilkinson.best/assets/music/busta.mp3",
 			color: "#cd4829"
 		},
 		{
